38d5ae0979f434cd8a21f9df1626a1cec4a7a68f
ulanav <ulanav@rambler.ru>
2019-01-09 16:46:58 +0300

    next  commit

--- WindowsFormsAppTest02/Form1.Designer.cs
+++ WindowsFormsAppTest02/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 316);
+            this.ClientSize = new System.Drawing.Size(478, 262);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
