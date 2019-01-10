eebeef94e234723cc34627fc7437dec9869712e7
ulanav <ulanav@rambler.ru>
2019-01-10 08:10:32 +0300

    Form Title changed

--- WindowsFormsAppTest02/Form1.Designer.cs
+++ WindowsFormsAppTest02/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(478, 344);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FormTestGit";
             this.ResumeLayout(false);
 
         }
